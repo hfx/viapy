085d35bcad8fec5a65f4761adeaf9c4fd2caee94
fx <herrmannfx@gmail.com>
2024-04-02 22:29:48 +0200

    fix: change url to path

--- viapy/test_urls.py
+++ viapy/test_urls.py
@@ -1,12 +1,12 @@
 """Test URL configuration for viapy
 """
-try:
-    from django.conf.urls import url, include
+try:    
+    from django.urls import path, include
 
     from viapy import urls as viapy_urls
 
     urlpatterns = [
-        url(r'^viaf/', include(viapy_urls, namespace='viaf')),
+        path('viaf/', include(viapy_urls, namespace='viaf')),
     ]
 
 except ImportError:
--- viapy/urls.py
+++ viapy/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from viapy.views import ViafLookup, ViafSearch
 
@@ -6,10 +6,10 @@ from viapy.views import ViafLookup, ViafSearch
 app_name = 'viapy'
 
 urlpatterns = [
-    url(r'^suggest/$', ViafLookup.as_view(), name='suggest'),
-    url(r'^suggest/person/$', ViafLookup.as_view(),
+    path('suggest/', ViafLookup.as_view(), name='suggest'),
+    path('suggest/person/', ViafLookup.as_view(),
         {'nametype': 'personal'}, name='person-suggest'),
-    url(r'^search/$', ViafSearch.as_view(), name='search'),
-    url(r'^search/person/$', ViafSearch.as_view(),
+    path('search/', ViafSearch.as_view(), name='search'),
+    path('search/person/', ViafSearch.as_view(),
         {'nametype': 'personal'}, name='person-search'),
 ]
